824e340aa085e120611db3748c75c58e3bf205d0
Brett Sevy <justbrettt@gmail.com>
2020-01-24 23:07:06 -0600

    adjusted the outcome of the password

--- script.js
+++ script.js
@@ -2,42 +2,37 @@
 var generateBtn = document.querySelector("#generate");
 
 
+
+
 //messages//
-alert("you need a new password");
+var writePassword =alert("you need a new password");
 var size = prompt('how long would you like your password? 8 - 128');
-var upper = confirm("do you want uppercase letters?");
-var lower = confirm('do you want lowercase letters?');
-var symbols = confirm("do you want any special characters?");
-var numbers = confirm('do you want numbers');
+//var upper = confirm("do you want uppercase letters?");
+//var lower = confirm('do you want lowercase letters?');
+//var symbols = confirm("do you want any special characters?");
+//var numbers = confirm('do you want numbers');
 
 
 
 //varibles//
-var upper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var upper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12344567890!@#$%^&*()");
 var lower = ("abcdefghijklmnopqrstuvwxyz");
 var numbers = ("1234567890");
 var symbols = ("!@#$%^&*()_+");
-
-
-console.log(password(size, upper))
-
 
 
 function password(size, upper) {
     var pwd ="";
 
     for (var i = 0; i < size; i++) {
-        pwd += lower.charAt(Math.floor(Math.random() * upper.length));
+        pwd += upper.charAt(Math.floor(Math.random() * upper.length));
         
     }
     
     return pwd;
 }
 
-
-
-var psswrdLength= ('randomnumber');
-
+console.log(password(size, upper))
 
 
 
@@ -47,6 +42,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  
 
 }
 
